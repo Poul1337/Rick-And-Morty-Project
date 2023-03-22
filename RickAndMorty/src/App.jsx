9f222a41013e8f25ee4941991f4e068a90639876
Poul <paweljarujaruszewski@gmail.com>
2023-03-22 19:04:45 +0100

    Navbar look

--- RickAndMorty/src/App.jsx
+++ RickAndMorty/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+import mainLogo from './img/mainLogo.png';
+import Buttons from './Components/Buttons';
+import ThemeSwitch from './Components/ThemeSwitch';
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log('BASE URL =>', import.meta.env.VITE_BASE_URL); // tak sie uzywa tego .env
+  const { data } = useQuery('BaseUrlFetch', () => {
+    return axios.get(import.meta.env.VITE_BASE_URL);
+  });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="navBar">
+        <img src={mainLogo} alt="Rick and Morty" />
+        <Buttons data={data?.data} />
+        <ThemeSwitch />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
